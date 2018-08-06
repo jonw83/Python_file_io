cf8328dd4a356a110205705b69acc979b5d13042
Jonathan Walters <jonfwalters@gmail.com>
2018-08-06 20:02:38 +0000

    Quiz walkthrough completed

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -6,6 +6,36 @@ def show_menu():
     option = input("Enter Option: ")
     return option
     
+def ask_questions():
+    questions = []
+    answers = []
+    
+    with open("questions.txt", "r") as file:
+        lines = file.read().splitlines()
+        
+    for i, text in enumerate(lines):
+        if i%2 == 0:
+            questions.append(text)
+        else:
+            answers.append(text)
+            
+    number_of_questions = len(questions)
+    questions_and_answers = zip(questions, answers)
+    
+    score = 0
+    
+    
+    for question, answer in questions_and_answers:
+        guess = input(question + "> ")
+        if guess == answer:
+            score +=1
+            print("Correct!")
+            print(score)
+        else:
+            print("Incorrect!")
+        
+    print("You got {0} correct out of {1}".format(score, number_of_questions))
+        
 def add_question():
     print("")
     question = input("Enter your question\n> ")
@@ -23,7 +53,7 @@ def game_loop():
     while True:
         option = show_menu()
         if option == "1":
-            print("You Selected 'Ask Questions'")
+            ask_questions()
         elif option == "2":
             add_question()
         elif option == "3":
